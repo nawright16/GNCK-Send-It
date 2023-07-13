feb596639a223283178f170d75ee168507389956
Casey Halbohn <wrxbeauty15@hotmail.com>
2023-07-12 21:09:08 -0400

    Added nav item list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav">
+        <div className="icons">
+          <ul>
+            {/* create chat */}
+            <li></li>
+            {/* friends */}
+            <li></li>
+            {/* chats */}
+            <li></li>
+            {/* notifications */}
+            <li></li>
+            {/* account/support */}
+            <li></li>
+            {/* toggle light/dark */}
+            <li></li>
+            {/* settings */}
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div className="aside">
+
+      </div>
+      <div className="main">
+
+      </div>
     </div>
   );
 }
