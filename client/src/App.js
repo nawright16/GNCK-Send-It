225f47933eee02180c8eda0c4b288c18a70321a4
Casey Halbohn <wrxbeauty15@hotmail.com>
2023-07-13 14:12:50 -0400

    Added IconButtons

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import * as React from 'react';
 import IconButton from '@mui/material/IconButton';
-import { CreateOutlined, ChatBubbleOutline } from '@mui/icons-material';
+import { CreateOutlined, ChatBubbleOutline, NotificationsOutlined, ViewComfyOutlined, SettingsOutlined } from '@mui/icons-material';
 import ChatWindow from './components/ChatWindow';
 import Container from "@mui/material/Container";
 
@@ -13,12 +13,14 @@ function App() {
           <ul>
             {/* create chat */}
             <li>
-              <IconButton size="medium">
+              <IconButton aria-label="create" color="pink" size="medium">
                 <CreateOutlined fontSize="medium" />
               </IconButton>
             </li>
             {/* friends */}
-            <li></li>
+            <li>
+
+            </li>
             {/* chats */}
             <li>
               <IconButton size="medium">
@@ -26,18 +28,33 @@ function App() {
               </IconButton>
             </li>
             {/* notifications */}
-            <li></li>
+            <li> <IconButton size="medium">
+              <NotificationsOutlined fontSize="medium" />
+            </IconButton>
+            </li>
             {/* account/support */}
-            <li></li>
+            <li>
+              <IconButton size="medium">
+                <ViewComfyOutlined fontSize="medium" />
+              </IconButton>
+            </li>
             {/* toggle light/dark */}
-            <li></li>
+            <li>
+
+            </li>
             {/* settings */}
-            {/* <li><SettingOulined fontSize="medium" /></li> */}
+            <li>
+              <IconButton size="medium">
+                <SettingsOutlined fontSize="medium" />
+              </IconButton>
+            </li>
           </ul>
         </div>
       </div>
       <div className="aside">
-
+        <div>
+          
+        </div>
       </div>
       <div className="main">
         <Container>
