27af71ed0d8ebc678c60ce5fb6810e624e9f5f44
GreggLedoux <guitlouie@gmail.com>
2023-07-15 09:06:28 -0400

    adding from Kristens code

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,63 @@
 // DEPENDENCIES
 const express = require("express")
-const http = require("http")
-const socketio = require("socket.io")
+const mongoose = require("mongoose")
 const cors = require("cors")
+const dotenv = require('dotenv');
+const socketio = require('socket.io');
+const connection = require('./db')
+require('dotenv').config
+
+
 
 // CONFIGURATION
-require('dotenv').config()
-const PORT = process.env.PORT || 3005
+const PORT = process.env.PORT || 5001
 const app = express()
-const server = http.createServer(app)
+const server = require('http').Server(app);
+const io = socketio(server);
+const axios = require('axios')
+const userRoutes = require("./routes/NewUser")
+const authRoutes = require("./routes/authorize")
 
-// ROUTES
-app.get('/', (req, res) => {
-  res.send('SendIt app response')
-})
 
-// LISTEN
-server.listen(PORT, () =>
-  console.log(`Server is up and running on port ${PORT}...`)
-)
-//app.listen(PORT, () => {
- // console.log('listening on port', PORT);
-//})
+//MongoDB Connection
+mongoose.connect("mongodb+srv://Send-It:aHAfCw9Tvqjsj000@cluster0.qwvybwq.mongodb.net/Send-It?retryWrites=true&w=majority")
+
+//db connection
+connection()
+
+//Middleware
+app.use(express.json())
+app.use(cors())
+
+// API ROUTES
+// app.get('/', (req, res) => {
+//   res.send('SendIt app response')
+// })
+
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes)
+
+
+//LISTEN
+const port = process.env.PORT || 5001
+// app.listen(PORT, () => {
+//   console.log('listening on port', PORT);
+// })
+
+// Start server
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+// Socket.IO
+io.on('connection', (socket) => {
+  console.log(`Socket ${socket.id} connected`);
+
+  socket.on('sendMessage', (message) => {
+    io.emit('message', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`Socket ${socket.id} disconnected`);
+  });
+});
