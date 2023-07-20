f503043ed0989c384a7d36c31c2d0b44e74cde1a
nawright16 <nawright16@gmail.com>
2023-07-20 18:10:01 -0400

    local chats working, changed userListItem and Singlechat, sarch still bad

--- frontend/src/components/userAvatar/UserListItem.js
+++ frontend/src/components/userAvatar/UserListItem.js
@@ -1,8 +1,10 @@
 import { Avatar } from "@chakra-ui/avatar";
 import { Box, Text } from "@chakra-ui/layout";
+import { ChatState } from "../../Context/ChatProvider";
 
 // UserListItem component displays an item in the user list
-const UserListItem = ({ user, handleFunction }) => {
+const UserListItem = ({ handleFunction }) => {
+  const { user } = ChatState();
   return (
     <Box
       onClick={handleFunction}
